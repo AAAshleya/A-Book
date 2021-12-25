3fb7637ac37aaa60500a6bb55f89364f9207d59f
AAAshley <554850368@qq.com>
2021-12-25 15:41:56 +0800

    Init

---
+++ src/test/java/com/aaashley/abook/ABookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaashley.abook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
